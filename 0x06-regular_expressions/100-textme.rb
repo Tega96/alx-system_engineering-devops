d2aa23c143c67a0e893c9a573e59e11736486471
Tega96 <otiteotega@gmail.com>
2024-05-09 15:22:26 -0700

    textme

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 #A real life regex problem
-puts ARGV[0].scan(/\[from (.*?)\] \[to (.*?)\] \[flag (.*?)\]/).join(",")
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
